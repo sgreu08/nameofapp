3d6bc6f4adb6705ceab234e8d35a231799ddb102
sgreu08 <s.greussing@zeppelin-university.net>
2015-07-29 11:51:50 +0200

    added price and size

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150728152920) do
+ActiveRecord::Schema.define(version: 20150729094727) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20150728152920) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "color"
+    t.string   "price"
+    t.string   "size"
   end
 
 end
