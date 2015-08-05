67a0d745e51c8a3be6efd060651e9e7a7c6b3d6d
sgreu08 <s.greussing@zeppelin-university.net>
2015-08-05 15:58:48 +0200

    changes form sample-app

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,22 @@
 class StaticPagesController < ApplicationController
-  layout "static"
-
-
+  
   def landing_page
     @featured_product = Product.first
-  end
-  def landing_page
+    # Display three products from your database
     @products = Product.limit(3)
+
+    @products = Product.all
+    # Define Counter Variable which is needed for automatically adopting the number of buttons to the number of slides on the landing page
+      @counter = Product.count  
+      @product = Product.new
+  end 
+
+  def index
+  end
+
+  def about
+  end
+
+  def contact
   end
 end
-
