c1a45c52b280c83ac4b12e8dfe16d46d288f82b7
sgreu08 <s.greussing@zeppelin-university.net>
2015-08-05 18:11:08 +0200

    root changes

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#index'
+  root 'static_pages#landing_page'
   resources :orders, only: [:index, :show, :new, :create]
 
   # Example of regular route:
