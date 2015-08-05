248949f62e7be278df5b99e575f145bfb968e04d
sgreu08 <s.greussing@zeppelin-university.net>
2015-08-05 16:16:58 +0200

    added landing_page in routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
+  get 'statig_pages/landing_page'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
