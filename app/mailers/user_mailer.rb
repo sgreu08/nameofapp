bf4c67ba9d4bfd4944760334fd45766f735240a2
sgreu08 <s.greussing@zeppelin-university.net>
2015-08-06 13:13:14 +0200

    email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
-class UserMailer < ApplicationMailer::Base
+class UserMailer < ActionMailer::Base
   default from: "from@example.com"
 
   def contact_form(email, name, message)
-        @message = message
-        mail(:from => email,
-                        :to => 'your-email@example.com',
-                        :subject => "A new contact form message from #{name}")
+  @message = message
+    mail(:from => email, 
+        :to => 'sonja.greussing@hotmail.com', 
+        :subject => "A new contact form message from #{name}")
   end
 end
