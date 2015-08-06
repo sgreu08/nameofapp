e2926e66e65ef4547adeb0b49b2361c4b6413dc7
sgreu08 <s.greussing@zeppelin-university.net>
2015-08-06 11:40:49 +0200

    lektion 38

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.stmp_settings = {
+ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
