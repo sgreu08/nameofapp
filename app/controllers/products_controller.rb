f30892a0dde9d2295a6c39d9c68c1de35155af7b
sgreu08 <s.greussing@zeppelin-university.net>
2015-08-12 17:57:16 +0200

    bug fixing

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,15 +1,26 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
-
-  before_filter :authentic_user!
-
-
-
+  # this is a comment
   # GET /products
   # GET /products.json
+  # added some comments to see if the branch thing works
+
+# code for entering the search field 
   def index
-    @products = Product.all
-    search_term = params[:q]
+    #conditional to see if the search term is present 
+    if params[:q]
+      search_term = params[:q]
+ # we can’t expect users to enter a name exactly as it is spelled 
+ # in the database, for this SQL has a useful matching operator called LIKE
+ # and it allows you to include a “wildcard” character (%) to indicate that 
+ # the matching term may be part of a longer string. 
+ # So we need to put percentage characters around our search_term string. 
+ # In order for us to do this, we need to use the string substitution method 
+ # that you learned in the Ruby primer, with the # character. 
+      @products = Product.where("name LIKE ?", "%#{search_term}%")
+    else
+      @products = Product.all
+    end
   end
 
   # GET /products/1
@@ -26,6 +37,9 @@ class ProductsController < ApplicationController
   # GET /products/1/edit
   def edit
   end
+
+  # Make sure that users need to login before they can see the products page
+  before_filter :authenticate_user!
 
   # POST /products
   # POST /products.json
