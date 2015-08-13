4ab578d1a9824f22d8175a31f91182127e3f0679
sgreu08 <s.greussing@zeppelin-university.net>
2015-08-13 12:21:13 +0200

    comments

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,8 +4,9 @@ class Product < ActiveRecord::Base
   has_many :comments
 
   def average_rating
-          comments.average(:raiting).to_f
+    comments.average(:rating).to_f
   end
+
 # Let's add a validation to the Product model that requires all new products
 # to have a name provided.
   validates :name, presence: true
